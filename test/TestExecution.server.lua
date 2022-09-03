173a75bf2a8dc162a47401e5e4b32156e659450e
Chipio Industries <53354220+ChipioIndustries@users.noreply.github.com>
2022-09-03 08:26:43 -0400

    Initial commit

---
+++ test/TestExecution.server.lua
@@ -0,0 +1,10 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
+
+TestEZ.TestBootstrap:run(
+	{
+		ReplicatedStorage.Packages.RandomSelection
+	},
+	TestEZ.Reporters.TextReporter
+)
